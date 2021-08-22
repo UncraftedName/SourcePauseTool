c33acff8a4a111b0dcc3e106875f6df26cd470aa
UncraftedName <uncraftedname@gmail.com>
2021-08-22 12:29:25 -0700

    more cleanup and add owned entities

--- spt/OrangeBox/overlay/sg-collision.cpp
+++ spt/OrangeBox/overlay/sg-collision.cpp
@@ -42,59 +42,35 @@
 #include "portal_camera.hpp"
 #include "..\..\utils\property_getter.hpp"
 
-// these structs are here in case I need to use them later, but I don't think I'll need them for anything
-/*struct Polyhedron_IndexedLine_t
-{
-	unsigned short iPointIndices[2];
-};
-
-struct Polyhedron_IndexedLineReference_t
-{
-	unsigned short iLineIndex;
-	unsigned char iEndPointIndex;
-};
-
-struct Polyhedron_IndexedPolygon_t
-{
-	unsigned short iFirstIndex;
-	unsigned short iIndexCount;
-	Vector polyNormal;
-};
-
-class CPolyhedron
-{
-public:
-	Vector* pVertices;
-	Polyhedron_IndexedLine_t* pLines;
-	Polyhedron_IndexedLineReference_t* pIndices;
-	Polyhedron_IndexedPolygon_t* pPolygons;
-
-	unsigned short iVertexCount;
-	unsigned short iLineCount;
-	unsigned short iIndexCount;
-	unsigned short iPolygonCount;
-};*/
-
-// void drawPolyhedrons(CUtlVector<CPolyhedron*> polyhedrons, int r, int g, int b) {}
-
-ConVar y_spt_draw_collision_wireframe("y_spt_draw_collision_wireframe", "1");
-
-void DrawCPhysCollide(const CPhysCollide* pCollide, const color32& c, const matrix3x4_t& mat, bool drawWireframe)
+void DrawCPhysCollide(const CPhysCollide* pCollide,
+                      const color32& c,
+                      const matrix3x4_t& mat,
+                      bool limitZFighting,
+                      bool drawFaces,
+                      bool drawWireframe)
 {
 	if (!pCollide)
 		return;
-	IMaterial* pMaterial =
-	    GetMaterialSystem()->FindMaterial("debug/debugtranslucentvertexcolor", TEXTURE_GROUP_OTHER);
+	const char* faceMatName = "debug/debugtranslucentvertexcolor";
+	IMaterial* faceMat = GetMaterialSystem()->FindMaterial(faceMatName, TEXTURE_GROUP_OTHER);
 	// DebugDrawPhysCollide uses CreateDebugMesh from vphysics, edit the mesh there
-	vphysicsDLL.adjustDebugMesh = true;
-	engineDLL.ORIG_DebugDrawPhysCollide(pCollide, pMaterial, mat, c, false);
+	vphysicsDLL.adjustDebugMesh = limitZFighting;
+	if (drawFaces)
+		engineDLL.ORIG_DebugDrawPhysCollide(pCollide, faceMat, mat, c, false);
 	if (drawWireframe)
-		engineDLL.ORIG_DebugDrawPhysCollide(pCollide, nullptr, mat, color32{0, 0, 0, 250}, false);
+	{
+		const color32& wireColor = drawFaces ? color32{0, 0, 0, 250} : c;
+		engineDLL.ORIG_DebugDrawPhysCollide(pCollide, nullptr, mat, wireColor, false);
+	}
 	vphysicsDLL.adjustDebugMesh = false;
 	return;
 }
 
-void DrawCPhysicsObject(const void* pPhysicsObject, const color32& c, bool drawWireframe)
+void DrawCPhysicsObject(const void* pPhysicsObject,
+                        const color32& c,
+                        bool limitZFighting,
+                        bool drawFaces,
+                        bool drawWireframe)
 {
 	if (!pPhysicsObject)
 		return;
@@ -103,38 +79,58 @@ void DrawCPhysicsObject(const void* pPhysicsObject, const color32& c, bool drawW
 	vphysicsDLL.ORIG_CPhysicsObject__GetPosition(pPhysicsObject, 0, &pos, &ang);
 	matrix3x4_t mat;
 	AngleMatrix(ang, pos, mat);
-	DrawCPhysCollide(*((CPhysCollide**)pPhysicsObject + 3), c, mat, drawWireframe);
+	DrawCPhysCollide(*((CPhysCollide**)pPhysicsObject + 3), c, mat, limitZFighting, drawFaces, drawWireframe);
 }
 
-void DrawCollisionForPortal(CBaseEntity* portal)
+void DrawCBaseEntity(const CBaseEntity* pEnt, const color32& c, bool limitZFighting, bool drawFaces, bool drawWireframe)
+{
+	if (!pEnt)
+		return;
+	DrawCPhysicsObject(*((void**)pEnt + 106), c, limitZFighting, drawFaces, drawWireframe);
+}
+
+ConVar y_spt_draw_collision_wireframe("y_spt_draw_collision_wireframe", "1");
+
+void DrawPortalEnv(CBaseEntity* portal)
 {
 	bool wire = y_spt_draw_collision_wireframe.GetBool();
 	matrix3x4_t mat;
 	AngleMatrix(vec3_angle, vec3_origin, mat);
 	uint32_t* simulator = (uint32_t*)portal + 327;
 
+	// portal hole - not used for collision (green wireframe)
+	DrawCPhysCollide(*(CPhysCollide**)(simulator + 70), color32{20, 255, 20, 255}, mat, false, false, wire);
 	// world brushes - wall geo in front of portal (red)
-	DrawCPhysCollide(*(CPhysCollide**)(simulator + 76), color32{255, 20, 20, 70}, mat, wire);
-	// local wall tube - the portal hole itself (probably not actually used for collision) (green)
-	DrawCPhysCollide(*(CPhysCollide**)(simulator + 94), color32{0, 255, 0, 200}, mat, wire);
+	DrawCPhysCollide(*(CPhysCollide**)(simulator + 76), color32{255, 20, 20, 70}, mat, true, true, wire);
+	// local wall tube - edge of portal (green)
+	DrawCPhysCollide(*(CPhysCollide**)(simulator + 94), color32{0, 255, 0, 200}, mat, true, true, wire);
 	// local wall brushes - wall geo behind the portal (blue)
-	DrawCPhysCollide(*(CPhysCollide**)(simulator + 101), color32{40, 40, 255, 60}, mat, wire);
-
+	DrawCPhysCollide(*(CPhysCollide**)(simulator + 101), color32{40, 40, 255, 60}, mat, true, true, wire);
 	// static props (piss colored)
 	const CUtlVector<char[28]>& clippedStaticProps = *(CUtlVector<char[28]>*)(simulator + 83);
 	for (int i = 0; i < clippedStaticProps.Count(); i++)
-		DrawCPhysCollide(*((CPhysCollide**)clippedStaticProps[i] + 2), color32{255, 255, 40, 50}, mat, wire);
+	{
+		const color32 c = color32{255, 255, 40, 50};
+		DrawCPhysCollide(*((CPhysCollide**)clippedStaticProps[i] + 2), c, mat, true, true, wire);
+	}
 
+	// if the linked sim is set we will have geo from the other portal
 	uint32_t* linkedSim = *(uint32_t**)(simulator + 1);
 	if (linkedSim)
 	{
 		AssertMsg(*(uint32_t**)(linkedSim + 1), "pointer to linked simulator makes no sense");
-		// remote brushes (white)
-		DrawCPhysicsObject(*(void**)(simulator + 103), color32{200, 200, 200, 20}, wire);
+		// remote brushes (white); z-fighting adjustment is set since this frequently overlaps with local world geo
+		DrawCPhysicsObject(*(void**)(simulator + 103), color32{200, 200, 200, 20}, true, true, wire);
 		// remote static props (light-yellow)
 		const CUtlVector<void*>& remoteStaticProps = *(CUtlVector<void*>*)(simulator + 104);
 		for (int i = 0; i < remoteStaticProps.Count(); i++)
-			DrawCPhysicsObject(remoteStaticProps[i], color32{255, 255, 100, 20}, wire);
+			DrawCPhysicsObject(remoteStaticProps[i], color32{255, 255, 100, 20}, false, true, wire);
+		// owned entities (pink wireframe)
+		const CUtlVector<CBaseEntity*>& ownedEntities = *(CUtlVector<CBaseEntity*>*)(simulator + 2171);
+		for (int i = 0; i < ownedEntities.Count(); i++)
+			DrawCBaseEntity(ownedEntities[i], color32{255, 100, 255, 50}, false, false, true);
+		// dynamic + 2048+5+5
+		// dynamic = remote static + 5 + 1 + 1 + 2
 	}
 
 	// TODO:
@@ -250,7 +246,8 @@ findPortal:
 	else
 	{
 		portalIndex = atoi(str);
-		if (portalIndex < 1 || portalIndex >= MAX_EDICTS || invalidPortal(utils::GetClientEntity(portalIndex)))
+		if (portalIndex < 1 || portalIndex >= MAX_EDICTS - 1
+		    || invalidPortal(utils::GetClientEntity(portalIndex)))
 			return; // given portal index isn't valid
 	}
 	lastPortalIndex = portalIndex;
@@ -263,5 +260,5 @@ findPortal:
 	if (!portal)
 		return;
 
-	DrawCollisionForPortal(portal->GetBaseEntity());
+	DrawPortalEnv(portal->GetBaseEntity());
 }
